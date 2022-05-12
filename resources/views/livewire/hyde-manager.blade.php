ee76710232c980a6eacd34bc546d2040dc090074
Caen De Silva <caen@desilva.se>
2022-05-12 13:05:54 +0200

    Tweak spacings

--- resources/views/livewire/hyde-manager.blade.php
+++ resources/views/livewire/hyde-manager.blade.php
@@ -2,7 +2,7 @@
     @if(! Buddy::hasHydeInstance())
     <div class="container mx-auto">
         <header class="text-center mb-4">
-            <h2>Hyde Manager</h2>
+            <h2>Hyde Setup Manager</h2>
             <p class="lead mb-2">
                 Could not find a Hyde installation. Let's get one set up!
             </p>
@@ -13,25 +13,29 @@
         </header>
         
         @if($formProgress >= 1)
-        <form class="container col-lg-6 mt-2" wire:submit.prevent="findHydeProject">
-            <div class="form-group">
-                <label for="path" class="h6">
-                    Please enter the full path to your Hyde project
-                    @if($formProgress > 1)
-                    ✅
-                    @endif
-                </label>
-                <div class="input-group " @if($formProgress > 1) title="Please refresh your page to change the path" @endif>
-                    <input type="text" class="form-control form-control-lg @error('path') is-invalid @enderror" id="path" wire:model="path" placeholder="e.g. C:\Users\Hyde\Desktop\MyNewHydeProject" required @disabled($formProgress > 1)>
-                    <button type="submit" @class([ 'my-0 btn', 'btn-primary' => $formProgress <= 1, 'btn-success' => $formProgress > 1, ]) @disabled($formProgress > 1)> Find Project </button>
+        <div class="container col-lg-6 py-2">
+                
+            <form wire:submit.prevent="findHydeProject">
+                <div class="form-group">
+                    <label for="path" class="h6">
+                        Please enter the full path to your Hyde project
+                        @if($formProgress > 1)
+                        ✅
+                        @endif
+                    </label>
+                    <div class="input-group " @if($formProgress > 1) title="Please refresh your page to change the path" @endif>
+                        <input type="text" class="form-control form-control-lg @error('path') is-invalid @enderror" id="path" wire:model="path" placeholder="e.g. C:\Users\Hyde\Desktop\MyNewHydeProject" required @disabled($formProgress > 1)>
+                        <button type="submit" @class([ 'my-0 btn', 'btn-primary' => $formProgress <= 1, 'btn-success' => $formProgress > 1, ]) @disabled($formProgress > 1)> Find Project </button>
+                    </div>
                 </div>
-            </div>
-            @error('path')
-            <div class="invalid-feedback d-block">
-                <strong>Error:</strong> {{ $message }}
-            </div>
-            @enderror
-        </form>
+                @error('path')
+                <div class="invalid-feedback d-block">
+                    <strong>Error:</strong> {{ $message }}
+                </div>
+                @enderror
+            </form>
+            
+        </div>
         @endif
     </div>
     @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         Welcome to {{ config('app.name') }}
     </x-slot>
     
-    <header class="px-4 py-5 my-5 text-center">
+    <header class="px-4 py-4 my-5 text-center">
         <h1 class="display-5 fw-bold">Welcome to {{ config('app.name') }}!</h1>
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4">
