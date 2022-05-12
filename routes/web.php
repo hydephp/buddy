5b90ad5dd2197628752f942d788d6af3adc965ad
Caen De Silva <caen@desilva.se>
2022-05-12 14:29:24 +0200

    Add RequiresInitializedHydeProject middleware

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/readme', ReadmeController::class)->name('readme');
-Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard')->middleware('initialized');
 
 Route::get('/debug', DebugController::class);
