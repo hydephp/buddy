76115f56f5f1b7d18882243baca32632e593f8ca
Caen De Silva <caen@desilva.se>
2022-05-12 10:36:03 +0200

    Update the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,12 @@
     <x-slot name="title">
         Welcome to {{ config('app.name') }}
     </x-slot>
+    
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1>
+                Welcome to {{ config('app.name') }}!
+            </h1>
+        </div>
+    </header>
 </x-app-layout>
