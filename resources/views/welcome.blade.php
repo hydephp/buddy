190f0d5633cf02514fb5f3b6ae6c54ef3f0a7fa1
Caen De Silva <caen@desilva.se>
2022-05-12 10:46:03 +0200

    Update welcome layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
         Welcome to {{ config('app.name') }}
     </x-slot>
     
-    <header class="p-5 mb-4 bg-light rounded-3">
-        <div class="container-fluid py-5">
-            <h1>
-                Welcome to {{ config('app.name') }}!
-            </h1>
+    <header class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold">Welcome to {{ config('app.name') }}!</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">
+                Bridging the gap between flatfile content managment and a traditional CMS.
+            </p>
         </div>
     </header>
 </x-app-layout>
