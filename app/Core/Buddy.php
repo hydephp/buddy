2ac82ccf17f0a6a2c1ea242af9635b8b351d56df
Caen De Silva <caen@desilva.se>
2022-05-12 11:56:21 +0200

    Sketch out the Buddy interface

--- app/Core/Buddy.php
+++ app/Core/Buddy.php
@@ -4,5 +4,15 @@ namespace App\Core;
 
 interface Buddy
 {
+    // Assertion methods
+    public function isInitialized(): bool;
+    public function hasHydeInstance(): bool;
 
+    // Initialization methods
+    public function initialize(): void;
+    public function constructHydeInstance(): void;
+
+    // Accessor methods
+    public function getHydeInstance(): Hyde;
+    public function hyde(): Hyde;
 }
--- app/Core/BuddyProvider.php
+++ app/Core/BuddyProvider.php
@@ -2,10 +2,60 @@
 
 namespace App\Core;
 
+/**
+ * Primary interface implementation for interacting with the Hyde Buddy.
+ */
 class BuddyProvider implements Buddy
 {
+    protected bool $initialized = false;
+    protected Hyde $hyde;
+
+
 	public function __construct()
 	{
-		
+        if (! $this->isInitialized())
+        {
+            $this->initialize();
+        }
 	}
+
+    // Assertion methods
+
+    public function isInitialized(): bool
+    {
+        return $this->initialized;
+    }
+
+    public function hasHydeInstance(): bool
+    {
+        return isset($this->hyde);
+    }
+
+    // Initialization methods
+
+    public function initialize(): void
+    {
+        $this->initialized = true;
+    }
+
+    public function constructHydeInstance(): void
+    {
+        $this->hyde = new Hyde();
+    }
+
+    // Accessor methods
+
+    public function hyde(): Hyde
+    {
+        return $this->getHydeInstance();
+    }
+
+    public function getHydeInstance(): Hyde
+    {
+        if (!$this->hasHydeInstance()) {
+            $this->constructHydeInstance();
+        }
+
+        return $this->hyde;
+    }
 }
