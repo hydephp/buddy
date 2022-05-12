cee4598f5c26529acef59cdfc198eaa345c3d909
Caen De Silva <caen@desilva.se>
2022-05-12 10:35:36 +0200

    Add Argon head contents

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,8 +1,32 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
+
         <title>{{ $title ?? config('app.name') }}</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+        <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.2') }}" rel="stylesheet" />
     </head>
     <body>
         {{ $slot }}
     </body>
 </html>
+
+<!--
+=========================================================
+* Argon Dashboard 2 - v2.0.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
