2121123c5018f050b6d48241136ed4a6d66aca12
Caen De Silva <caen@desilva.se>
2022-05-12 20:17:20 +0200

    Broadcast and listen for serverStatus change

--- app/Http/Livewire/HydeServerStatus.php
+++ app/Http/Livewire/HydeServerStatus.php
@@ -8,8 +8,9 @@ use Livewire\Component;
 class HydeServerStatus extends Component
 {
     public bool $loaded = false;
-
     public int $status = 0;
+
+    protected $listeners = ['serverStatus'];
 
     public function ping()
     {
@@ -18,10 +19,17 @@ class HydeServerStatus extends Component
             $response = Http::timeout(3)->get('http://localhost:8080/');
         } catch (\Illuminate\Http\Client\ConnectionException $exception) {
             $this->loaded = true;
+            $this->emit('serverStatus', 0);
             return;
         }
         $this->status = $response->status();
+        $this->emit('serverStatus', $response->status());
         $this->loaded = true;
+    }
+
+    public function serverStatus($status)
+    {
+        $this->status = $status;
     }
 
     public function render()
