2a074e27777467b491edfcfcf6bad5a6724890e3
Caen De Silva <caen@desilva.se>
2022-05-12 13:40:53 +0200

    Remove remote font

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,7 +7,6 @@
 
         <title>{{ $title ?? config('app.name') }}</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
         <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
         <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.2') }}" rel="stylesheet" />
 
