b05ec507452eb0fb3d1d5b41a4bcb88889f160ad
Caen De Silva <caen@desilva.se>
2022-05-12 16:41:45 +0200

    Remove nucleo

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,7 +7,6 @@
 
         <title>{{ $title ?? config('app.name') }}</title>
 
-        <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
         <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.2') }}" rel="stylesheet" />
 
         <link href="{{ asset('app.css') }}" rel="stylesheet" />
