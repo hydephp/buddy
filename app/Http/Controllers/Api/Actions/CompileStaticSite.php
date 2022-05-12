fce8388f4b819eab10c7497891ec9e694490e9e3
Caen De Silva <caen@desilva.se>
2022-05-12 21:32:44 +0200

    Change white-space to pre-line

--- app/Http/Controllers/Api/Actions/CompileStaticSite.php
+++ app/Http/Controllers/Api/Actions/CompileStaticSite.php
@@ -21,14 +21,14 @@ class CompileStaticSite extends Controller
         // @todo add lock to prevent multiple requests
         $time_start = microtime(true);
         ob_end_clean();
-        echo '<pre>';
+        echo '<pre style="white-space:pre-line;">';
         echo '<h1>Compiling static site</h1>';
         echo '<h2>Initiating build loop</h2>';
         echo '<h3 style="color:red;">This may take a while... Feed might seem unresponsive. Do not reload or close this window.</h3>';
         echo '<h4>Your application terminal may contain extra debug output.</h4>';
         $this->passthru('build --no-api'); // @todo allow toggling of api calls
 
-        echo "\n<h2>Done. Finished in " . round((microtime(true) - $time_start) * 1000, 2). "ms</h2>\n";
+        echo "\n<h2>Done. Finished in " . round((microtime(true) - $time_start) * 1000, 2). "ms</h2>";
         echo '<h3 style="color:green;">It is now safe to close this window.</h3><button onclick=window.close();>Close Window</button>';
     }
 }
