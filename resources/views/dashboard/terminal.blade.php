9f00afe3b3d744dcf4b8e27050681cc4d566591b
Caen De Silva <caen@desilva.se>
2022-05-13 13:51:27 +0200

    Add warning banner and extra information

--- resources/views/dashboard/terminal.blade.php
+++ resources/views/dashboard/terminal.blade.php
@@ -3,18 +3,25 @@
         {{ config('app.name') }} Terminal
     </x-slot>
     
-    <header class="px-4 py-4 my-5 text-center">
+    <header class="px-4 py-4 mt-5 text-center">
         <h1 class="text-3xl font-bold">
             HydeCLI Terminal Preview
         </h1>
         
+        <div class="alert alert-warning mt-3" role="alert">
+            <strong>Warning!</strong> Do not run the serve command in this terminal as it will cause the PHP server to halt.
+        </div>
+
         <p class=" text-gray-600">
 			The Terminal Preview currently only proxies commands and returns the output.
-			It does not work with interactive commands.
+			It does not work with interactive commands. You should also not run commands
+            that do not stop automatically like the `serve` command, as the terminal runs
+            in the same process as the application which will cause Buddy to freeze.
 		</p>
+
     </header>
 
-	<div class="col-xl-8 mx-auto p-3">
+	<div class="col-12 mx-auto p-3 mb-5">
 		<livewire:terminal />
 	</div>
 </x-app-layout>
