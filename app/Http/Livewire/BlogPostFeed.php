c1fb3ad39fc806a53dde1b610a4f10951a2ab197
Caen De Silva <caen@desilva.se>
2022-05-13 17:33:11 +0200

    Load the post feed

--- app/Http/Livewire/BlogPostFeed.php
+++ app/Http/Livewire/BlogPostFeed.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\MarkdownPost;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class BlogPostFeed extends Component
 {
+    /**
+     * @var Collection<MarkdownPost>
+     */
+    public Collection $posts;
+
     public function mount()
     {
-        
-    }
-
-    public function load()
-    {
-        
+        $this->posts = Hyde::getLatestPosts()->values();
     }
 
     public function render()
--- resources/views/livewire/blog-post-feed.blade.php
+++ resources/views/livewire/blog-post-feed.blade.php
@@ -1,3 +1,25 @@
-<div wire:init="load">
-    
+<div>
+    <table class="table">
+		<thead>
+			<tr>
+				<th scope="col" class="p-2">Title</th>
+				<th scope="col" class="p-2">Author</th>
+				<th scope="col" class="p-2">Date</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($posts as $post)
+				<tr>
+					<td>
+						{{ $post->title }}
+					</td>
+					<td>
+						{{ $post->author->username }}
+					</td>
+					<td>
+						{{ $post->date->short }}
+					</td>
+				</tr>
+			@endforeach
+	</table>
 </div>
