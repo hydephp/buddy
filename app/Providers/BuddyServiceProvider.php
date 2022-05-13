f0880c8c8ab49b179e48ddf159d5106d5ed5723e
Caen De Silva <caen@desilva.se>
2022-05-13 23:03:01 +0200

    Save configuration singleton

--- app/Providers/BuddyServiceProvider.php
+++ app/Providers/BuddyServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Core\BuddyProvider;
+use App\Core\ConfigurationManager;
 use App\Core\Contracts\Buddy;
+use App\Core\Contracts\BuddyConfiguration;
 use Illuminate\Support\ServiceProvider;
 
 class BuddyServiceProvider extends ServiceProvider
@@ -17,6 +19,10 @@ class BuddyServiceProvider extends ServiceProvider
     {
         $this->app->singleton(Buddy::class, function ($app) {
             return new BuddyProvider();
+        });
+
+        $this->app->singleton(BuddyConfiguration::class, function ($app) {
+            return new ConfigurationManager();
         });
     }
 
