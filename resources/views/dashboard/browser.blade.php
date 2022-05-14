a21d342ebf9439a092cd763b32c252344f49838c
Caen De Silva <caen@desilva.se>
2022-05-14 17:13:18 +0200

    Remove buttons I can't get to work due to CORS

--- resources/views/dashboard/browser.blade.php
+++ resources/views/dashboard/browser.blade.php
@@ -16,7 +16,6 @@
 		#toolbar {
 			display: flex;
 			align-items: center;
-			padding-left: 0;
 			list-style-type: none;
 			background: #353535;
 			color: white;
@@ -42,6 +41,9 @@
 			width: auto;
 			display: flex;
 			flex: 1;
+			color: white;
+			padding: 1px 10px;
+			font-size: 14px;
 		}
 		#refresh {
 			transition: transform 0.2s ease-in-out;
@@ -52,16 +54,6 @@
 		<section class="container">
 			<header>
 				<menu id="toolbar" type="toolbar">
-					<li>
-						<button>
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-						</button>
-					</li>
-					<li>
-						<button>
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
-						</button>
-					</li>
 					<li>
 						<button id="refresh" onclick="refresh()" style="transform: rotate(0deg)">
 							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
@@ -75,7 +67,7 @@
 						</a>
 					</li>
 					<li style="display: flex; flex: 1;">
-						<input id="uri" type="text" readonly>
+						<input id="uri" type="text" value="localhost:8080" readonly>
 					</li>
 					<li>
 						<button>
@@ -85,13 +77,14 @@
 					</li>
 				</menu>
 			</header>
-			<iframe id="browser" name="browser" src="http://localhost:8080/" class="col-12" style="min-height: 75vh; background: lightgray;"></iframe>		
+			<iframe id="browser" name="browser" src="http://localhost:8080/" class="col-12" style="min-height: 75vh;"></iframe>		
 		</section>
 	</div>
 
 	<script>
 		let browser = document.getElementById('browser');
 		let refreshButton = document.getElementById('refresh');
+		let uri = document.getElementById('uri');
 		function refresh() {
 			// var uri = document.getElementById('uri').value;
 			browser.src = browser.src;
