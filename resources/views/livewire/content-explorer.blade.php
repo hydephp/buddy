c7a6cb56bdd5e8fd9deb646a37c8607dd14b9c85
Caen De Silva <caen@desilva.se>
2022-06-05 22:37:12 +0200

    Create the markdown page table

--- resources/views/livewire/content-explorer.blade.php
+++ resources/views/livewire/content-explorer.blade.php
@@ -1,6 +1,11 @@
 <div wire:init="load">
     @if($loaded)
         <div>
+            <style>
+                .table tbody tr:last-child td {
+                    border-width: 0 1px 1px 1px;
+                }
+            </style>
             <section>
                 <h6>Blade Pages</h6>
                 @php($bladePages = collect($pages->get('bladePages')))
@@ -33,7 +38,26 @@
                 @php($markdownPages = collect($pages->get('markdownPages')))
                 @if($markdownPages->count() > 0)
                     <table class="table table-bordered">
-                        @dump($markdownPages)
+                        <thead>
+                            <tr>
+                                <th>Page Title</th>
+                                <th>Slug</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($markdownPages as  $markdownPage)
+                                @php($markdownPage = (object) $markdownPage)
+                                <tr>
+                                    <td>{{ $markdownPage->title }}</td>
+                                    <td>{{ $markdownPage->slug }}</td>
+                                    <td>
+                                        <a class="btn btn-link btn-xs mb-0" href="/_site/{{ $markdownPage->slug }}">See live page<sup>NYI</sup></a>
+                                        <a class="btn btn-link btn-xs mb-0" href="/app/markdown-editor?page={{ urlencode('_pages/'.$markdownPage->slug.'.md') }}">Edit markdown<sup>NYI</sup></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 @else
                     <p>No Markdown Pages Found</p>
