115df8f0e4433daec3cfd8e425a015c13dd45c73
Caen De Silva <caen@desilva.se>
2022-06-05 21:36:20 +0200

    Cache the collection saving about 500ms (98.4%)
    Caches collections for five minutes, as parsing the pages is an expensive operation.
    For the DocsCI, making the request take around 8ms instead of 500ms

--- app/Http/Livewire/ContentExplorer.php
+++ app/Http/Livewire/ContentExplorer.php
@@ -7,7 +7,9 @@ use Hyde\Framework\Models\BladePage;
 use Hyde\Framework\Models\DocumentationPage;
 use Hyde\Framework\Models\MarkdownPage;
 use Hyde\Framework\Models\MarkdownPost;
+use Hyde\Framework\Services\CollectionService;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 use Desilva\Console\Console;
 
@@ -30,6 +32,16 @@ class ContentExplorer extends Component
         $console = new Console;
         $console->debug('Loading page collections...');
 
+        $hash = $this->getContentHash();
+
+        if (Cache::has('content-explorer-'.$hash)) {
+            $this->pages = Cache::get('content-explorer-' . $hash);
+            $console->debug('Loaded page collections from cache in '
+                . round((microtime(true) - $time) * 1000, 2) . 'ms.');
+            $this->loaded = true;
+            return;
+        }
+
         $this->pages = new Collection();
 
         $bladePages = BladePage::all();
@@ -42,7 +54,8 @@ class ContentExplorer extends Component
         $this->pages->put('markdownPosts', $markdownPosts);
         $this->pages->put('documentationPages', $documentationPages);
 
-        $console->debug('Loaded all pages in ' . round(microtime(true) - $time, 2) . ' seconds.');
+        Cache::put('content-explorer-' . $hash, $this->pages, now()->addMinutes(5));
+        $console->debug('Loaded all pages in ' . round((microtime(true) - $time) * 1000, 2) . 'ms.');
 
         $this->loaded = true;
     }
@@ -50,5 +63,15 @@ class ContentExplorer extends Component
     public function render()
     {
         return view('livewire.content-explorer');
+    }
+
+    protected function getContentHash(): string
+    {
+        return md5(implode('', array_merge([
+            implode('', CollectionService::getBladePageList()),
+            implode('', CollectionService::getMarkdownPageList()),
+            implode('', CollectionService::getMarkdownPostList()),
+            implode('', CollectionService::getDocumentationPageList()),
+        ])));
     }
 }
