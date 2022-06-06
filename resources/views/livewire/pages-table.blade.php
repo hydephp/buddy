380da58e4b7d3af4bf6440befdd8a0b940c675b7
Caen De Silva <caen@desilva.se>
2022-06-06 12:13:59 +0200

    Add table styling

--- resources/views/livewire/pages-table.blade.php
+++ resources/views/livewire/pages-table.blade.php
@@ -1,22 +1,27 @@
 <div>
+    <style>
+        .ptc1 {
+            width: 1%;
+        }
+    </style>
     @if (count($pages) > 0)
         <table class="table table-bordered table-padding-1">
             <thead>
                 <tr>
-                    <th>Type</th>
+                    <th class="ptc1">Type</th>
                     <th>File name</th>
-                    <th>Actions</th>
+                    <th class="text-end">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($pages as $type => $subpages)
                     @foreach ($subpages as $page)
                     <tr>
-                        <td>{{ $pageNames[$type] }}</td>
+                        <td class="ptc1">{{ $pageNames[$type] }}</td>
                         <td>{{ $page }}.{{ $type === 'bladePage' ? 'blade.php' : 'md' }}</td>
-                        <td>
-                            <a href="#not-yet-implemented">View</a>
-                            <a href="#not-yet-implemented">Edit</a>
+                        <td class="text-end">
+                            <a class="mx-2" href="#not-yet-implemented">View</a>
+                            <a class="mx-2" href="#not-yet-implemented">Edit</a>
                         </td>
                     </tr>
                     @endforeach
