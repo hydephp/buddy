c61a5fc60ca78e9cc6095f327dfe6e4b11e1012a
Caen De Silva <caen@desilva.se>
2022-06-06 13:15:38 +0200

    Return the view

--- app/Http/Controllers/MarkdownFileViewer.php
+++ app/Http/Controllers/MarkdownFileViewer.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Hyde\Framework\Hyde;
 use Illuminate\Http\Request;
 
 class MarkdownFileViewer extends Controller
 {
     public function show(string $directory, string $file, Request $request)
     {
-        //
+        $path = $directory .'/'. $file;
+        abort_unless(file_exists(Hyde::path($path)), 404);
+
+        $content = file_get_contents(Hyde::path($path));
+
+        return view('dashboard.markdown-file-page', [
+            'content' => $content,
+            'path' => $path,
+            'directory' => $directory,
+            'filename' => $file,
+        ]);
     }
 }
