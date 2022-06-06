323e39e87616c9698adcb6c325bd868445f61064
Caen De Silva <caen@desilva.se>
2022-06-06 13:30:17 +0200

    Add Markdown Contents

--- resources/views/dashboard/markdown-file-page.blade.php
+++ resources/views/dashboard/markdown-file-page.blade.php
@@ -3,7 +3,7 @@
         {{ config('app.name') }} Markdown File Viewer
     </x-slot>
     
-    <div class="col-xxl-10">
+    <div class="col-12 container d-flex flex-wrap justify-content-center">
         <header class="px-4 py-4 mt-5 text-center">
             <h1 class="text-3xl font-bold">
                 Markdown File Viewer
@@ -12,5 +12,19 @@
 				Showing <code>{{ $path }}</code>
 			</p>
         </header>
+
+		<section class="card">
+			<header class="card-header pb-2 pt-3 bg-light">
+				<h4>Markdown Contents</h4>
+			</header>
+			<div class="card-body p-3 pt-2 markdown-contents bg">
+				<pre><code class="language-markdown">{{ $content }}</code></pre>
+			</div>
+
+			{{-- TODO load locally instead of CDN --}}
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.0/build/styles/default.min.css">
+			<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.0/build/highlight.min.js"></script>
+			<script>hljs.highlightAll();</script>
+		</section>
     </div>
 </x-app-layout>
